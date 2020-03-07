08f186b389944433efca276cdfd96c355f697e09
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-07 01:25:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,8 +31,8 @@ def can_be_created_in_a_block
   release_date = arg[:release_date]
 
   Movie.create do |m|
-    m.title = title
-    m.release_date = release_date
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
