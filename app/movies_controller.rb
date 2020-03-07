a26decb577f1f0795023c20c545c550c53e943d6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-07 01:20:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,11 +29,10 @@ def can_be_created_in_a_block(title = "Home Alone", release_date = 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = titlem.release_date = release_date
+    m.title = title
+    m.release_date = release_date
   end
-  Movie.create do |m|
-    
-  end
+
 end
 
 def can_get_the_first_item_in_the_database
